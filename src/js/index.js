76a94fb979fa7049f57aa23eac91e3302964a4cd
Bartosz Gębka <gebkabartosz@gmail.com>
2022-11-05 14:12:39 +0100

    move accordion.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import Accordion from "./accordion";
+import Accordion from "./accordion/accordion";
 
 const accordionEls = document.querySelectorAll(".accordion");
 accordionEls.forEach((el) => new Accordion(el));
