993db8469d710150c572914a61a09104dc15b37b
Bartosz Gębka <gebkabartosz@gmail.com>
2023-01-19 10:58:46 +0100

    test commit

--- src/js/components/spinner/spinner.js
+++ src/js/components/spinner/spinner.js
@@ -15,7 +15,7 @@ export class Spinner extends Component {
 	block() {
 		this.#blockScreenEl = this.#createBlockScreen();
 		this.reload(this.#blockScreenEl);
-		this.#blockScreenEl.classList.add(this.#CLASS_NAME_SHOW);
+		this.#blockScreenEl.clasList.add(this.#CLASS_NAME_SHOW);
 	}
 
 	#createBlockScreen() {
